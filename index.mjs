d95597b32d4e1681967c7b1e9446fdf44a54bc48
Marharyta Malets <marharyta_malets@epam.com>
2025-05-01 20:06:30 +0300

    feat: add application exits if user pressed ctrl+c or sent .exit command and proper message is printed

--- index.mjs
+++ index.mjs
@@ -1,5 +1,26 @@
 import parseArgs from './src/cli/args.js';
+import readline from "readline";
 
 const args = parseArgs();
-console.log(`Welcome to the File Manager, ${args.username ?? 'Username'}!`);
+const userName = args.username ?? 'Username';
 
+console.log(`Welcome to the File Manager, ${userName}!`);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line',(input) => {
+  switch (input.trim()) {
+    case '.exit': rl.close();
+  }
+})
+
+rl.on('close', () => {
+  console.log(`\nThank you for using File Manager, ${userName}, goodbye!`);
+})
+
+process.on("SIGINT", () => {
+  rl.close()
+});
